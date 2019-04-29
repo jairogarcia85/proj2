0fee01b295a7de93899ade15d819ec448245e90f
Jairo Garcia <jgarcia@indratek.com>
2019-04-29 10:32:44 -0500

    segunda comprobación de ramás

--- handlers/passport.js
+++ handlers/passport.js
@@ -1,9 +1,9 @@
-const passport = require('passport')
-const User = require('../models/User')
+const passport = require("passport");
+const User = require("../models/User");
 
-passport.use(User.createStrategy())
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-
-module.exports = passport
+//se exporta
+module.exports = passport;
